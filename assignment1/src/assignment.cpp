2b7636b7240b27400d0833aa3caef8b2604dad5a
nwihardjo <nwihardjo@connect.ust.hk>
2019-03-13 01:17:07 +0800

    finalised codes

--- assignment1/src/assignment.cpp
+++ assignment1/src/assignment.cpp
@@ -10,7 +10,10 @@ using namespace std;
 const char* STUDENT_ID = "20315011";
 
 // TODO: define your global variables here if you need
-
+token_feedforward_classifier* model_;
+int embedding_size=128;
+int num_hidden_layers=1;
+int num_epochs = 1;
 /**
  * initialize your model. this function will be called before the "train" function
  * \param vocab the vocabulary, represented as a list of unique strings
@@ -19,7 +22,7 @@ const char* STUDENT_ID = "20315011";
 void init(const std::vector<std::string>& vocab, const std::vector<std::string>& labels) {
   // TODO: do whatever necessary to initialize your model
   // hint: you need to choose an appropriate embedding size and number of hidden layers
-
+	model_ = new token_feedforward_classifier(vocab, embedding_size, num_hidden_layers, labels);
 }
 
 /**
@@ -30,7 +33,7 @@ void init(const std::vector<std::string>& vocab, const std::vector<std::string>&
 void train(const std::vector<std::string> &tokens, const std::vector<std::string> &oracles) {
   // TODO: complete this training function
   // hint: you need to choose an appropriate number of epochs
-
+	model_->train(tokens, oracles, num_epochs); 
 }
 
 /**
@@ -40,5 +43,5 @@ void train(const std::vector<std::string> &tokens, const std::vector<std::string
  */
 std::vector<std::string> test(const std::vector<std::string> &tokens) {
   // TODO: complete this testing function
-
+	return model_->test(tokens);
 }
