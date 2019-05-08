01e66ae5e8873f11eb10ea077f5f20253dcf4065
nwihardjo <nwihardjo@connect.ust.hk>
2019-05-09 01:56:40 +0800

    completed part b

--- assignment3/part_a/analysis.py
+++ assignment3/part_a/analysis.py
@@ -21,36 +21,3 @@ for n in tree.findall(".//token[@type]"):
 
 print dict
 print "Total token", count
-
-
-'''
-dict_ = {}
-clash = []
-number = []
-with_dot = []
-email_and_links = []
-for n in tree.findall(".//nonterminal[@value]"):
-	if "alt." in n.text and len(n.text) > 1:
-		with_dot.append(n.text)
-
-	if "@" in n.text or "://" in n.text:
-		email_and_links.append(n.text)
-
-	if n.text not in dict_:
-		dict_[n.text] = n.get("value")
-	elif dict_[n.text] != n.get("value"):
-		clash.append([n.text, dict_[n.text], n.get("value")])
-	if hasNumbers(n.text) == True:
-		number.append(n.text)
-
-#pprint(clash)
-print "Word having two different POS tags:", len(clash), "out of", len(dict_), "words"
-
-#pprint(number)
-print "Token which contain any number: ", len(number)
-
-#pprint(with_dot)
-print "Token which have weird dot etc: ", len(with_dot)
-
-print "Token with email or any url : ", len(email_and_links)
-'''
--- assignment3/part_b/assignment.cpp
+++ assignment3/part_b/assignment.cpp
@@ -6,29 +6,15 @@
 #include <shared_utils.hpp>
 
 namespace part_b{
+/*
+  // ==========================
+  // ITERATION 1 - HYPOTHESIS 1
+  // ==========================
+
   const unsigned NUM_EPOCHS = 5;
 
-  /**
-   * create your custom classifier by combining transducers
-   * the input to your classifier will a list of tokens
-   * when creating your custom classifier, the training set is passed as a parameter
-   * this is because you need to assemble your vocabulary from training set
-   * \param training_set the training set that your classifier will train on
-   * \param postags the list of all POS tags.
-   * \param iobes_tags the list of IOBES tags. it contains "I" "O" "B" "E" "S" (but not necessarily in order)
-   * \return
-   */
-  transducer_t your_classifier(const vector<sentence_t> &training_set, const vector<symbol_t> &postags,
-                               const vector<symbol_t> &iobes_tags) {
+  transducer_t your_classifier(const vector<sentence_t> &training_set, const vector<symbol_t> &postags, const vector<symbol_t> &iobes_tags) {
 
-    // in this starting code, we demonstrates how to construct a 2-layer feedforward neural network
-    // that takes the target token and the POS tag of the target token as input
-
-    // first you need to assemble the vocab you need
-    // in this simple model, the vocab is the top 1000 most frequent tokens in training set
-    // we provide a frequent_token_collector utility,
-    // that can count token frequencies and collect the top X most frequent tokens
-    // all out-of-vocabulary tokens will be treated as "unknown token"
     frequent_token_collector vocab_collector;
     for (const auto &sentence:training_set) {
       for (const auto &token:sentence) {
@@ -37,10 +23,8 @@ namespace part_b{
     }
     vector<symbol_t> vocab = vocab_collector.list_frequent_tokens(1000);
 
-    // create an embedding lookup layer for token input
     auto embedding_lookup = make_embedding_lookup(64, vocab);
 
-    // the size of postag vocabulary are small, a onehot layer will work just fine
     auto postag_onehot = make_onehot(postags);
 
     auto concatenate = make_concatenate(2);
@@ -51,23 +35,193 @@ namespace part_b{
 
     auto onehot_inverse = make_onehot_inverse(iobes_tags);
 
-    return compose(group(embedding_lookup, embedding_lookup), concatenate, dense0, dense1, onehot_inverse);
+	return compose(embedding_lookup, dense1, onehot_inverse);
   }
 
-  /**
-   * besides the target token to chunk, your model may also need other "context" input
-   * this function defines the inputs that your model expects
-   * \param sentence the sentence where the token is in
-   * \param postags the POS tags of the sentence (predicted by your model)
-   * \param target_index the position of the target token to chunk
-   * \return
-   */
   vector<feature_t>
   get_features(const vector<symbol_t> &sentence, const vector<symbol_t> &postags, unsigned target_index) {
+	return vector<feature_t>{postags[target_index]};
+  }
+  
+  // ==========================
+  // ITERATION 1 - HYPOTHESIS 2
+  // ==========================
 
-    // TODO: define what input to feed to your classifier
-    // this starting code demonstrates how to define input as:
-    // the target token, the target token's POS tag
+  const unsigned NUM_EPOCHS = 40;
+
+  transducer_t your_classifier(const vector<sentence_t> &training_set, const vector<symbol_t> &postags, const vector<symbol_t> &iobes_tags) {
+
+    frequent_token_collector vocab_collector;
+    for (const auto &sentence:training_set) {
+      for (const auto &token:sentence) {
+        vocab_collector.add_occurence(token);
+      }
+    }
+    vector<symbol_t> vocab = vocab_collector.list_frequent_tokens(1000);
+
+    auto embedding_lookup = make_embedding_lookup(128, vocab);
+
+    auto postag_onehot = make_onehot(postags);
+
+    auto concatenate = make_concatenate(3);
+
+    auto dense0 = make_dense_feedfwd(64, make_tanh());
+
+    auto dense1 = make_dense_feedfwd(iobes_tags.size(), make_softmax());
+
+    auto onehot_inverse = make_onehot_inverse(iobes_tags);
+
+    return compose(group(embedding_lookup, embedding_lookup, embedding_lookup), concatenate, dense1, onehot_inverse);
+  }
+
+  vector<feature_t> get_features(const vector<symbol_t> &sentence, const vector<symbol_t> &postags, unsigned target_index) {
+    
+    feature_t prev = (target_index > 0) ? postags[target_index-1] : "<s>";
+    feature_t next = (target_index < sentence.size() - 1) ? postags[target_index+1] : "<s>";
+
+    return vector<feature_t>{prev, postags[target_index], next};
+  }
+
+  // ==========================
+  // ITERATION 1 - HYPOTHESIS 3
+  // ==========================
+
+  const unsigned NUM_EPOCHS = 30;
+
+  transducer_t your_classifier(const vector<sentence_t> &training_set, const vector<symbol_t> &postags, const vector<symbol_t> &iobes_tags) {
+
+    frequent_token_collector vocab_collector;
+    for (const auto &sentence:training_set) {
+      for (const auto &token:sentence) {
+        vocab_collector.add_occurence(token);
+      }
+    }
+    vector<symbol_t> vocab = vocab_collector.list_frequent_tokens(1000);
+
+    auto embedding_lookup = make_embedding_lookup(128, vocab);
+
+    auto postag_onehot = make_onehot(postags);
+
+    auto concatenate = make_concatenate(2);
+
+    auto dense0 = make_dense_feedfwd(64, make_tanh());
+
+    auto dense1 = make_dense_feedfwd(iobes_tags.size(), make_softmax());
+
+    auto onehot_inverse = make_onehot_inverse(iobes_tags);
+
+    return compose(group(embedding_lookup, embedding_lookup), concatenate, dense1, onehot_inverse);
+  }
+
+  vector<feature_t> get_features(const vector<symbol_t> &sentence, const vector<symbol_t> &postags, unsigned target_index) {
+    
     return vector<feature_t>{sentence[target_index], postags[target_index]};
+  }
+
+  // ==========================
+  // ITERATION 1 - HYPOTHESIS 4 
+  // ==========================
+  const unsigned NUM_EPOCHS = 45;
+
+  transducer_t your_classifier(const vector<sentence_t> &training_set, const vector<symbol_t> &postags, const vector<symbol_t> &iobes_tags) {
+    frequent_token_collector vocab_collector;
+    for (const auto &sentence:training_set) {
+      for (const auto &token:sentence) {
+        vocab_collector.add_occurence(token);
+      }
+    }
+    vector<symbol_t> vocab = vocab_collector.list_frequent_tokens(1000);
+
+    auto embedding_lookup = make_embedding_lookup(128, vocab);
+    auto postag_onehot = make_onehot(postags);
+    auto concatenate = make_concatenate(6);
+    auto dense0 = make_dense_feedfwd(64, make_tanh());
+    auto dense1 = make_dense_feedfwd(iobes_tags.size(), make_softmax());
+    auto onehot_inverse = make_onehot_inverse(iobes_tags);
+
+    return compose(group(embedding_lookup, embedding_lookup, embedding_lookup, embedding_lookup, embedding_lookup, embedding_lookup), concatenate, dense0, dense1, onehot_inverse);
+  }
+
+  vector<feature_t> get_features(const vector<symbol_t> &sentence, const vector<symbol_t> &postags, unsigned target_index) {
+    feature_t next;
+    feature_t next_pos;
+    feature_t prev;
+    feature_t prev_pos;
+
+    if (target_index > 0) {
+	prev = sentence[target_index-1];
+	prev_pos = postags[target_index-1];
+    } else {
+	prev = "<s>";
+	prev_pos = "<s>";
+    }
+
+    if (int(target_index) < int(sentence.size())-1) {
+	next = sentence[target_index+1];
+	next_pos = postags[target_index+1];
+    } else {
+	next = "<s>";
+	next_pos = "<s>";
+    }
+    return vector<feature_t>{prev, sentence[target_index], next, prev_pos, postags[target_index], next_pos};
+  }
+
+
+*/ 
+  // ==========================
+  // ITERATION 2 - HYPOTHESIS 4 
+  // ==========================
+  const unsigned NUM_EPOCHS = 25;
+
+  transducer_t your_classifier(const vector<sentence_t> &training_set, const vector<symbol_t> &postags, const vector<symbol_t> &iobes_tags) {
+
+    frequent_token_collector vocab_collector;
+    for (const auto &sentence:training_set) {
+      for (const auto &token:sentence) {
+        vocab_collector.add_occurence(token);
+      }
+    }
+    vector<symbol_t> vocab = vocab_collector.list_frequent_tokens(1000);
+
+    auto embedding_lookup = make_embedding_lookup(256, vocab);
+
+    auto postag_onehot = make_onehot(postags);
+
+    auto concatenate = make_concatenate(8);
+
+    auto dense0 = make_dense_feedfwd(64, make_tanh());
+
+    auto dense1 = make_dense_feedfwd(iobes_tags.size(), make_softmax());
+
+    auto onehot_inverse = make_onehot_inverse(iobes_tags);
+
+    return compose(group(embedding_lookup, embedding_lookup, embedding_lookup, embedding_lookup, embedding_lookup, embedding_lookup, embedding_lookup, embedding_lookup), concatenate, dense1, onehot_inverse);
+  }
+
+  vector<feature_t> get_features(const vector<symbol_t> &sentence, const vector<symbol_t> &postags, unsigned target_index) {
+    feature_t next;
+    feature_t next_pos;
+    feature_t prev;
+    feature_t prev_pos;
+
+    feature_t prev1 = (target_index > 1) ? sentence[target_index-2] : "<s>";
+    feature_t next1 = (int(target_index) < int(sentence.size())-2) ? sentence[target_index+2] : "<s>";
+
+    if (target_index > 0) {
+	prev = sentence[target_index-1];
+	prev_pos = postags[target_index-1];
+    } else {
+	prev = "<s>";
+	prev_pos = "<s>";
+    }
+
+    if (int(target_index) < int(sentence.size())-1) {
+	next = sentence[target_index+1];
+	next_pos = postags[target_index+1];
+    } else {
+	next = "<s>";
+	next_pos = "<s>";
+    }
+    return vector<feature_t>{prev1, prev, sentence[target_index], next, next1, prev_pos, postags[target_index], next_pos};
   }
 }
